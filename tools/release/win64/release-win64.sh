e778c416c1ba1146991a3ffb62105c7a31786797
Anton Yemelyanov <anton.yemelyanov@gmail.com>
2016-08-06 07:41:44 -0400

    Adding optional different origins for project snapshot.

--- tools/release/win64/release-win64.sh
+++ tools/release/win64/release-win64.sh
@@ -11,16 +11,35 @@ NODE_PATH="/c/Program Files/nodejs"
 cd ../../../..
 mkdir releases
 cd releases
+
+echo "Cleaning up..."
 rm -rf $SIA_RELEASE_PATH
 
+echo "Cloning..."
 
-mkdir $SIA_RELEASE_PATH
-cp -r ../sia-cluster/* $SIA_RELEASE_PATH/
+if test "$1" == "--local"; then
+	echo "Packaging LOCAL copy"
+	mkdir $SIA_RELEASE_PATH
+	cp -r ../sia-cluster/* $SIA_RELEASE_PATH/
+	cd $SIA_RELEASE_PATH
+elif test "$1" == "--dev"; then
+	echo "Packaging DEV branch"
+	git clone https://github.com/aspectron/sia-cluster $SIA_RELEASE_PATH
+	cd $SIA_RELEASE_PATH
+	git checkout -b dev
+	git branch --set-upstream-to=origin/dev dev
+	git pull
+	npm install
+else
+	echo "Packaging MASTER branch"
+	git clone https://github.com/aspectron/sia-cluster $SIA_RELEASE_PATH
+	cd $SIA_RELEASE_PATH
+	npm install
+fi
 
-#git clone https://github.com/aspectron/sia-cluster $SIA_RELEASE_PATH
+# we are in $SIA_RELEASE_PATH
 
-cd $SIA_RELEASE_PATH
-# npm install
+echo "Binaries..."
 
 mkdir bin
 cd bin
@@ -32,9 +51,9 @@ cp -r  "$MONGO_PATH/bin/mongodump.exe" mongo/
 cp -r  "$MONGO_PATH/bin/libeay32.dll" mongo/
 cp -r  "$MONGO_PATH/bin/ssleay32.dll" mongo/
 
-mkdir mingw64
-cp -r  "$MINGW_PATH/bin/curl.exe" mingw64/
-cp -r  "$MINGW_PATH/bin/libcurl-4.dll" mingw64/
+#mkdir mingw64
+#cp -r  "$MINGW_PATH/bin/curl.exe" mingw64/
+#cp -r  "$MINGW_PATH/bin/libcurl-4.dll" mingw64/
 
 mkdir node
 cp -r  "$NODE_PATH/node.exe" node/
@@ -49,9 +68,18 @@ cd ../../bin
 
 echo -e "@echo off\ncd ..\nbin\\\\node\\\\node tools/release/win64/init.js %1\ncd bin\npause\n" > init.bat
 
-cd ../..
+cd ..
+rm bash.exe.stackdump
+cd ..
 
-/c/Program\ Files/WinRAR/WinRAR a -r -afzip sia-cluster-$SIA_CLUSTER_VERSION.zip $SIA_RELEASE_PATH/*
+echo "Packaging..."
+#/c/Program\ Files/WinRAR/WinRAR a -r sia-cluster-$SIA_CLUSTER_VERSION.rar $SIA_RELEASE_PATH/*
+#/c/Program\ Files/WinRAR/WinRAR a -r -afzip sia-cluster-$SIA_CLUSTER_VERSION.zip $SIA_RELEASE_PATH/*
+#/c/Program\ Files/Git/mingw64/zip -r sia-cluster-$SIA_CLUSTER_VERSION.zip $SIA_RELEASE_PATH/*
+zip -q -r sia-cluster-$SIA_CLUSTER_VERSION.zip $SIA_RELEASE_PATH/*
+
+
+echo "Done."
 
 popd
 
