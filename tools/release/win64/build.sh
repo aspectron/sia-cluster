288d097c4117bd39b113ac7a57e7dacb34fdf5ca
Anton Yemelyanov <anton.yemelyanov@gmail.com>
2016-08-07 06:12:31 -0400

    Generalizing setup scripts to be platform neutral. Working bundle.sh

--- tools/release/win64/build.sh
+++ tools/release/win64/build.sh
@@ -4,8 +4,6 @@ pushd .
 
 SIA_CLUSTER_VERSION="v0-9-2"
 SIA_RELEASE_PATH="sia-cluster-$SIA_CLUSTER_VERSION"
-NODE_VERSION="4.4.7"
-MONGO_PATH="/c/Program Files/MongoDB/Server/3.2"
 MINGW_PATH="/c/Program Files/Git/mingw64"
 NODE_PATH="/c/Program Files/nodejs"
 
@@ -34,24 +32,24 @@ if [[ $* == *--local* ]]; then
 elif [[ $* == *--dev* ]]; then
 	echo "Packaging DEV branch"
 	git clone https://github.com/aspectron/sia-cluster $SIA_RELEASE_PATH
-	if (($? != 0)); then echo "GIT CLONE Error" && exit; fi
+	if (($? != 0)); then echo "GIT CLONE Error" && exit 1; fi
 	cd $SIA_RELEASE_PATH
 	git checkout -b dev
-	if (($? != 0)); then echo "GIT CHECKOUT Error" && exit; fi
+	if (($? != 0)); then echo "GIT CHECKOUT Error" && exit 1; fi
 	git branch --set-upstream-to=origin/dev dev
 	sleep 3
 	git pull
-	if (($? != 0)); then echo "GIT PULL Error" && exit; fi
+	if (($? != 0)); then echo "GIT PULL Error" && exit 1; fi
 	npm install
-	if (($? != 0)); then echo "NPM Error" && exit; fi
-	flatten-packagesz
+	if (($? != 0)); then echo "NPM Error" && exit 1; fi
+	flatten-packages
 elif [[ $* == *--master* ]]; then
 	echo "Packaging MASTER branch"
 	git clone https://github.com/aspectron/sia-cluster $SIA_RELEASE_PATH
-	if (($? != 0)); then echo "GIT CLONE Error" && exit; fi
+	if (($? != 0)); then echo "GIT CLONE Error" && exit 1; fi
 	cd $SIA_RELEASE_PATH
 	npm install
-	if (($? != 0)); then echo "NPM Error" && exit; fi
+	if (($? != 0)); then echo "NPM Error" && exit 1; fi
 	flatten-packages
 fi
 
@@ -61,13 +59,6 @@ echo "Binaries..."
 
 mkdir -p bin
 cd bin
-
-mkdir mongo
-cp -r  "$MONGO_PATH/bin/mongod.exe" mongo/
-cp -r  "$MONGO_PATH/bin/mongo.exe" mongo/
-cp -r  "$MONGO_PATH/bin/mongodump.exe" mongo/
-cp -r  "$MONGO_PATH/bin/libeay32.dll" mongo/
-cp -r  "$MONGO_PATH/bin/ssleay32.dll" mongo/
 
 #mkdir mingw64
 #cp -r  "$MINGW_PATH/bin/curl.exe" mingw64/
@@ -81,7 +72,7 @@ mkdir -p data/db
 touch data/db/.db
 cd bin
 
-echo -e "@echo off\ncd ..\nbin\\\\node\\\\node tools/release/win64/init.js %1\ncd bin\npause\n" > setup.bat
+echo -e "@echo off\ncd ..\nbin\\\\node\\\\node tools/release/win64/setup.js %1\ncd bin\npause\n" > setup.bat
 
 cd ../..
 
@@ -90,6 +81,7 @@ if [[ ! $* == *--nopack* ]]; then
 	#/c/Program\ Files/WinRAR/WinRAR a -r sia-cluster-$SIA_CLUSTER_VERSION.rar $SIA_RELEASE_PATH/*
 	#/c/Program\ Files/WinRAR/WinRAR a -r -afzip sia-cluster-$SIA_CLUSTER_VERSION.zip $SIA_RELEASE_PATH/*
 	#/c/Program\ Files/Git/mingw64/zip -r sia-cluster-$SIA_CLUSTER_VERSION.zip $SIA_RELEASE_PATH/*
+	rm sia-cluster-$SIA_CLUSTER_VERSION.zip
 	zip -q -9 -r sia-cluster-$SIA_CLUSTER_VERSION.zip $SIA_RELEASE_PATH/*
 fi
 
--- tools/release/win64/bundle.sh
+++ tools/release/win64/bundle.sh
@@ -13,21 +13,29 @@ cd releases
 
 if [[ ! $* == *--nobuild* ]]; then
 	cd ../sia-cluster/tools/release/win64
-	bash build-win64.sh $1 --nopack
+	bash build.sh $1
+	if (($? != 0)); then echo "Sia Cluster Build Error" && exit 1; fi
 	cd ../../../../sia-node/tools/release/win64
-	bash build-win64.sh $1 --nopack
-	cd ../../../..
+	bash build.sh $1
+	if (($? != 0)); then echo "Sia Node Build Error" && exit 1; fi
+	cd ../../../../releases
 fi
 
-if [ ! -d SIA_CLUSTER_RELEASE_PATH]; then exit; fi
-if [ ! -d SIA_NODE_RELEASE_PATH]; then exit; fi
+if [[ ! -d "$SIA_CLUSTER_RELEASE_PATH" ]]; then
+	echo "Bundle error: Unable to locate $SIA_CLUSTER_RELEASE_PATH"
+	exit
+fi
+if [[ ! -d "$SIA_NODE_RELEASE_PATH" ]]; then 
+	echo "Bundle error: Unable to locate $SIA_NODE_RELEASE_PATH"
+	exit 
+fi
 
 echo "Packaging Bundle..."
 #/c/Program\ Files/WinRAR/WinRAR a -r sia-cluster-$SIA_CLUSTER_VERSION.rar $SIA_RELEASE_PATH/*
 #/c/Program\ Files/WinRAR/WinRAR a -r -afzip sia-cluster-$SIA_CLUSTER_VERSION.zip $SIA_RELEASE_PATH/*
 #/c/Program\ Files/Git/mingw64/zip -r sia-cluster-$SIA_CLUSTER_VERSION.zip $SIA_RELEASE_PATH/*
-echo "zip -q -9 -r xx-sia-cluster-$SIA_CLUSTER_VERSION.zip $SIA_CLUSTER_RELEASE_PATH/* -i $SIA_NODE_RELEASE_PATH/*"
 
+rm sia-cluster-bundle-$SIA_CLUSTER_VERSION.zip
 zip -q -9 -r sia-cluster-bundle-$SIA_CLUSTER_VERSION.zip $SIA_CLUSTER_RELEASE_PATH $SIA_NODE_RELEASE_PATH
 
 
